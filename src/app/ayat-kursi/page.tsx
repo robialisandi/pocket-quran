1a20d651fcd62674fd38a6d4e250dd948b2df8f9
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2023-09-29 00:16:23 +0700

    style: add font lpmq for arabic text

--- src/app/ayat-kursi/page.tsx
+++ src/app/ayat-kursi/page.tsx
@@ -4,7 +4,7 @@ export default function AyatKursiPage() {
   return (
     <div className="flex flex-col items-center px-4">
       <h1 className="flex p-4 font-bold text-2xl">Ayat Kursi</h1>
-      <span className="text-right font-bold font-arabic text-2xl text-[#2F6742]">
+      <span className="text-right font-arabic text-2xl text-[#2F6742]">
         {ayatKursi.arabic}
       </span>
       <small className="text-sm text-foreground-secondary italic">
