9418f46f54dcef14efa7c4fb5039543eeeb67e3c
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2023-09-29 00:17:35 +0700

    refactor: add new component FrameAyat

--- src/components/CardSurah.tsx
+++ src/components/CardSurah.tsx
@@ -1,5 +1,6 @@
 import { SurahInfoItem } from '@/data/surah-info'
 import Link from 'next/link'
+import FrameAyat from './FrameAyat'
 
 export default function CardSurah({
   surahInfoItem,
@@ -11,13 +12,7 @@ export default function CardSurah({
       <div className="flex flex-col justify-center gap-4 p-5 border md:rounded-xl bg-white my-0.5">
         <div className="flex justify-between items-center">
           <div className="flex items-start gap-4">
-            <div className="frame-ayah flex justify-center items-center mt-[5px]">
-              <span className="font-bold text-sm pt-[4px] text-white">
-                {surahInfoItem.index.toLocaleString('ar-u-nu-arab', {
-                  useGrouping: false,
-                })}
-              </span>
-            </div>
+            <FrameAyat number={surahInfoItem.index} />
             <div className="flex flex-col">
               <span className="font-bold text-md">{surahInfoItem.latin}</span>
               <span className="text-xs text-gray-500">
@@ -25,7 +20,7 @@ export default function CardSurah({
               </span>
             </div>
           </div>
-          <small className="text-2xl text-[#2F6742] font-bold font-arabic text-right">
+          <small className="text-2xl text-[#2F6742] font-arabic text-right">
             {surahInfoItem.arabic}
           </small>
         </div>
