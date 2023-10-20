e63f7857e462d3e80ebd8d899151f5061dbeeef6
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2023-10-21 00:44:36 +0700

    feat: add icons menus

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,37 @@ export default function Home() {
   const menus = [
     {
       name: 'Surah',
-      icon: 'surah',
+      icon: '1.surah.svg',
       pathName: '/surah',
     },
     {
       name: 'Juz Amma',
-      icon: 'juz-amma',
+      icon: '2.juz-amma.svg',
       pathName: '/juz-amma',
     },
     {
       name: 'Doa-doa',
-      icon: 'doa',
+      icon: '3.doa-doa.svg',
       pathName: '/doa',
     },
     {
       name: 'Wirid',
-      icon: 'wirid',
+      icon: '4.wirid.svg',
       pathName: '/wirid',
     },
     {
       name: 'Tahlil',
-      icon: 'tahlil',
+      icon: '5.tahlil.svg',
       pathName: '/tahlil',
     },
     {
       name: 'Ayat Kursi',
-      icon: 'ayat-kursi',
+      icon: '6.ayat-kursi.svg',
       pathName: '/ayat-kursi',
     },
     {
       name: 'Asma Allah',
-      icon: 'asmaul-husna',
+      icon: '7.asma-allah.svg',
       pathName: '/asmaul-husna',
     },
   ]
