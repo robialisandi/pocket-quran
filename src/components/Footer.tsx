487d612fb4266359523c3a500b98439e537813cf
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2024-02-25 15:33:57 +0700

    refactor: footer narasi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,42 +10,33 @@ export default function Footer() {
     <footer className="text-center p-4 text-gray-400">
       <h6>-</h6>
       <small>
-        Ngaaaji menggunakan sumber data dari{' '}
-        <a
-          href="https://github.com/rioastamal/quran-json"
-          target="_blank"
-          className="text-[#2F6742]"
-        >
+        Ngaaaji menggunakan sumber data Al-Qur'an dari{' '}
+        <a href="https://github.com/rioastamal/quran-json" target="_blank" className="text-[#2F6742]">
           Quran Json
         </a>{' '}
-        oleh Rio Astamal untuk sumber data ayat-ayat Al Qur'an.{' '}
+        yang disusun oleh Rio Astamal. Untuk doa-doa harian, kami mengandalkan{' '}
         <a
           href="https://www.doaharianislami.com/2017/06/kumpulan-doa-sehari-hari-lengkap-dalam-bahasa-arab-latin-dan-artinya.html"
           target="_blank"
           className="text-[#2F6742]"
         >
           Doa Harian Islami
-        </a>{' '}
-        untuk data doa-doa harian dan{' '}
+        </a>
+        , sementara bacaan tahlil, wirid, dan Asmaul Husna kami peroleh dari{' '}
         <a
           href="https://islam.nu.or.id/syariah/susunan-bacaan-tahlil-doa-arwah-lengkap-dan-terjemahannya-drr3t"
           target="_blank"
           className="text-[#2F6742]"
         >
           islam.nu.or.id
-        </a>{' '}
-        untuk bacaan tahlil, wirid dan{' '}
-        <a
-          href="https://islam.nu.or.id/ubudiyah/99-asmaul-husna-dan-artinya-1T8jl"
-          target="_blank"
-          className="text-[#2F6742]"
-        >
-          Asmaul Husna
         </a>
+        . Dengan inspirasi dari{' '}
+        <a href="https://github.com/mazipan" target="_blank" className="text-[#2F6742]">
+          Maz Ipan
+        </a>
+        , kami mengucapkan terima kasih kepada semua pihak yang terlibat, baik langsung maupun tidak langsung.
       </small>
-      <p className="text-sm mt-5">
-        Versi Aplikasi: {publicRuntimeConfig?.version}
-      </p>
+      <p className="text-sm mt-5">Versi Aplikasi: {publicRuntimeConfig?.version}</p>
       <small>Copyright © 2023 - {currentYear}</small>
     </footer>
   )
