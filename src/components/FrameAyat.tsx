1801952f8be5e3bc858bc9d27b0a19870859a166
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2024-02-29 14:08:51 +0700

    refactor: frame ayat

--- src/components/FrameAyat.tsx
+++ src/components/FrameAyat.tsx
@@ -1,7 +1,7 @@
-export default function FrameAyat({ number }: { number: number }) {
+export default function FrameAyat({ number, isPlaying = false }: { number: number; isPlaying?: boolean }) {
   return (
-    <div className="frame-ayah flex justify-center items-center mt-[5px]">
-      <span className="font-bold text-sm pt-[4px] text-white">
+    <div className={`frame-ayah ${isPlaying ? 'frame-ayah-color' : ''} flex justify-center items-center mt-[5px]`}>
+      <span className={`font-bold text-[16px] pt-[4px] text-white`}>
         {number.toLocaleString('ar-u-nu-arab', {
           useGrouping: false,
         })}
