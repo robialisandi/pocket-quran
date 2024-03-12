58f60579a1467f3595054629b7d16f4660c3d91b
Robi Alisandi <robi.alisandi@sekolahmu.co.id>
2024-03-12 19:05:47 +0700

    refactor: surah

--- src/hook/useSurah.tsx
+++ src/hook/useSurah.tsx
@@ -1,19 +1,43 @@
-import { useCallback, useEffect, useState } from 'react'
+'use client'
 
-export default function useSurah(surahId: string) {
-  const [surah, setSurah] = useState<any>({})
+import { useEffect, useState } from 'react'
+import surathInfo, { SurahInfoItem, type SurahInfo } from '@/data/surah-info'
+import MakkiyahMadaniyah from '@/data/makkiyah-madaniyah'
 
-  const getSurah = useCallback(async (surahId: string) => {
-    const res = await fetch(`/api/surah/${surahId}`, {
-      next: { revalidate: 10 },
-    })
-    const data = await res.json()
-    setSurah(data)
-  }, [])
+export function useSurahListSearch(): [SurahInfoItem[], string, (query: string) => void, boolean, boolean] {
+  const [searchText, setSearchText] = useState<string>('')
+  const [filterSurahInfo, setFilterSurahInfo] = useState<SurahInfo>({})
 
   useEffect(() => {
-    getSurah(surahId)
-  }, [getSurah, surahId])
+    const normalizeString = (str: string) => str.replace(/[^a-zA-Z0-9 ]/g, '').toLowerCase()
 
-  return surah
+    const filterSurah = () => {
+      let result: SurahInfo = {}
+      for (const [_, surah] of Object.entries(surathInfo)) {
+        const revelation = MakkiyahMadaniyah[surah.index]
+        const latinName = normalizeString(surah.latin)
+        const translation = normalizeString(surah.translation)
+
+        const queryNormalize = normalizeString(searchText)
+        if (latinName.includes(queryNormalize) || translation.includes(queryNormalize)) {
+          result[surah.index] = {
+            ...surah,
+            revelation: revelation ? revelation : 0,
+          }
+        }
+      }
+      return result
+    }
+
+    setFilterSurahInfo(filterSurah())
+  }, [searchText])
+
+  const handleSearch = (query: string) => {
+    setSearchText(query)
+  }
+
+  const isLoading = Object.keys(filterSurahInfo).length === 0 && searchText.length === 0
+  const isEmpty = Object.keys(filterSurahInfo).length === 0 && searchText.length !== 0
+
+  return [Object.values(filterSurahInfo), searchText, handleSearch, isLoading, isEmpty]
 }
